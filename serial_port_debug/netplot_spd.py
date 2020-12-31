1d07e4b585d25f9e6de523dac9e0ce4798cb2708
Paul Austen <pausten.os@gmail.com>
2020-12-31 06:45:15 +0000

    Handle -ve values.

--- serial_port_debug/netplot_spd.py
+++ serial_port_debug/netplot_spd.py
@@ -86,7 +86,7 @@ class Plotter(object):
 class SerialDebugger(object):
     """@brief Provide serial deug capabilities using netplot"""
 
-    DECIMAL_DIGIT_LIST = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+    DECIMAL_DIGIT_LIST = ('-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
     HEXADECIMAL_DIGIT_LIST = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F')
     WHITESPACE_CHARS = ( " ", "\t" )
 
